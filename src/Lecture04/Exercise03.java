37eaa95e2b46e5afeb57b97470e671075d07e733
denislav-a-velinov <denislav.a.velinov@gmail.com>
2024-02-05 16:52:18 +0200

    changes

--- src/Lecture04/Exercise03.java
+++ src/Lecture04/Exercise03.java
@@ -6,29 +6,24 @@ public class Exercise03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please, enter unit price: ");
-        int productUnitPrice = Integer.parseInt(scanner.nextLine());
+        double productUnitPrice = scanner.nextDouble();
         System.out.println("Please, enter quantity of the product: ");
-        int productQuantity = Integer.parseInt(scanner.nextLine());
+        int productQuantity = (int) scanner.nextDouble();
         System.out.println("Your product price per unit is: " + productUnitPrice + "$");
         System.out.println("Your product quantity is: " + productQuantity + "pcs");
-        int totalPrice = productQuantity * productUnitPrice;
+        double totalPrice = (int) (productQuantity * productUnitPrice);
         System.out.println("Your total is: " + totalPrice + "$");
 
+        double discount = 0;
         if (productQuantity >= 100 && productQuantity <= 120) {
             System.out.println("Your discount rate is 15%");
         } else if (productQuantity > 120) {
             System.out.println("Your discount rate is 20%");
         } else {
             System.out.println("Your discount rate is 0%");
-        }
-
-        double discount = 0;
-        if (productQuantity >= 100 && productQuantity <= 120) {
+        } if (productQuantity >= 100 && productQuantity <= 120) {
             discount = 0.15 * totalPrice;
-        } else if (productQuantity > 120) {
-            discount = 0.20 * totalPrice;
-        }
-
+        } else if (productQuantity > 120) discount = 0.20 * totalPrice;
         double revenueTotal = totalPrice - discount;
 
         System.out.println("The revenue from the sale is: " + revenueTotal + "$");
